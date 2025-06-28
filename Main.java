22fe672aaed391e858002f9956c04c8d3922e498
God Xero <godxero.dev@gmail.com>
2025-06-28 22:25:03 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import tableprinter.TablePrinter;
-
-public class Main {
-	public static void main(String[] args) {
-		final TablePrinter tblPrinter = new TablePrinter();
-
-		System.out.println(tblPrinter);
-	}
-}
