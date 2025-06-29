a69eb26d8ce58f22358868f821ae4d348fc80d0e
GodXero <godxero.dev@gmail.com>
2025-06-29 22:56:06 +0530

    make getTable static method. Make getTable meothd more flexible

--- tableprinter/TablePrinter.java
+++ tableprinter/TablePrinter.java
@@ -9,9 +9,13 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TablePrinter  {
-	public TablePrinter() {}
+	private TablePrinter() {}
 
-	private List<Integer> getMaxWidths(List<String> headers, List<List<String>> data) {
+	private static TablePrinterSettings getDefaultSettings() {
+		return new TablePrinterSettings();
+	}
+
+	private static List<Integer> getMaxWidths(List<String> headers, List<List<String>> data) {
 		final int cols = headers.size();
 		final int rows = data.size();
 		final List<Integer> output = IntStream.range(0, cols).mapToObj(_ -> 0).collect(Collectors.toList());
@@ -31,7 +35,9 @@ public class TablePrinter  {
 		return output;
 	}
 
-	public String getTable(Class<?> className, List<?> instances, List<String> columnHeaders) {
+	private static String getTable(Class<?> className, List<?> instances, List<String> columnHeaders, TablePrinterSettings settings) {
+		if (settings == null) settings = TablePrinter.getDefaultSettings();
+
 		final List<String> declaredFields = Arrays.asList(className.getDeclaredFields()).stream().map(field -> field.getName()).toList();
 		final List<String> methodNames = declaredFields.stream().map(field -> "get" + Character.toUpperCase(field.charAt(0)) + field.substring(1)).toList();
 		final List<String> headers = columnHeaders == null ? declaredFields : columnHeaders;
@@ -57,13 +63,13 @@ public class TablePrinter  {
 			data.add(row);
 		}
 
-		final List<Integer> maxWidths = this.getMaxWidths(headers, data);
+		final List<Integer> maxWidths = TablePrinter.getMaxWidths(headers, data);
 		final String hr = "+%s+".formatted(maxWidths.stream().map(width -> "-".repeat(width + 2)).collect(Collectors.joining("+")));
 		final StringBuilder outputBuilder = new StringBuilder(hr);
 
 		outputBuilder.append("\n| ");
 
-		IntStream.range(0, cols).mapToObj(i -> i).collect(Collectors.toList()).forEach(i -> outputBuilder.append(headers.get(i) + " ".repeat(maxWidths.get(i) - headers.get(i).length()) + " |" + (i == cols - 1 ? "" : " ")));
+		IntStream.range(0, cols).boxed().toList().forEach(i -> outputBuilder.append(headers.get(i)).append(" ".repeat(maxWidths.get(i) - headers.get(i).length())).append(" |").append(i == cols - 1 ? "" : " "));
 
 		outputBuilder.append('\n');
 		outputBuilder.append(hr);
@@ -71,7 +77,7 @@ public class TablePrinter  {
 		data.forEach(row -> {
 			outputBuilder.append("\n| ");
 
-			IntStream.range(0, cols).mapToObj(i -> i).collect(Collectors.toList()).forEach(i -> outputBuilder.append(row.get(i) + " ".repeat(maxWidths.get(i) - row.get(i).length()) + " |" + (i == cols - 1 ? "" : " ")));
+			IntStream.range(0, cols).boxed().toList().forEach(i -> outputBuilder.append(row.get(i)).append(" ".repeat(maxWidths.get(i) - row.get(i).length())).append(" |").append(i == cols - 1 ? "" : " "));
 
 			outputBuilder.append('\n');
 			outputBuilder.append(hr);
@@ -80,7 +86,32 @@ public class TablePrinter  {
 		return outputBuilder.toString();
 	}
 
-	public String getTable(Class<?> className, List<?> instances) {
-		return this.getTable(className, instances, null);
+	@SuppressWarnings("unchecked")
+	public static String getTable(Class<?> className, List<?> instances, Object... params) {
+		if (params.length == 0) return TablePrinter.getTable(className, instances, TablePrinter.getDefaultSettings(), null);
+
+		if (params.length == 1) {
+			if (params[0] instanceof TablePrinterSettings) return TablePrinter.getTable(className, instances, null, params[0]);
+			return TablePrinter.getTable(className, instances, params[0], null);
+		}
+
+		TablePrinterSettings settings = null;
+		List<String> headers = null;
+
+		if (params[0] instanceof TablePrinterSettings tablePrinterSettings) {
+			settings = tablePrinterSettings;
+			headers = (List<String>) params[1];
+		}
+
+		if (params[1] instanceof TablePrinterSettings tablePrinterSettings){
+			headers = (List<String>) params[0];
+			settings = tablePrinterSettings;
+		}
+
+		return TablePrinter.getTable(className, instances, headers, settings == null ? TablePrinter.getDefaultSettings() : settings);
+	}
+
+	public static class TablePrinterSettings {
+		public TablePrinterSettings() {}
 	}
 }
